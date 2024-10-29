db8efdb70d5f5f35176ac87aea07dec8e0b583dc
Skyler Robbins <ssssssssssssss>
2024-10-28 21:24:17 -0600

    Completed Lab

---
+++ ISpy/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  ISpy
+//
+//  Created by Skyler Robbins on 10/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        scrollView.delegate = self
+        updateZoomForSize(size: view.bounds.size)
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func updateZoomForSize(size: CGSize) {
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        let scale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = scale
+        scrollView.zoomScale = scale
+    }
+
+}
+
